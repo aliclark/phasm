c2b85489f1c9abe08988b44f9761539f24534c25
Ali Clark <ali@clark.gb.net>
2015-04-02 01:29:02 +0100

    semi-formed idea

--- phasm.sh
+++ phasm.sh
@@ -8,6 +8,8 @@ curdir="$(dirname "$(readlink -f "$0")")"
 exe=$(mktemp)
 
 if [[ $script_name == "gh:"* ]]; then
+    # XXX: we *could* chmod +x the resulting script, since we know it should be
+    # executable.
     echo "Import(\"$script_name\")" | $curdir/compiler.py >$exe
 else
     $curdir/compiler.py <$script_name >$exe
